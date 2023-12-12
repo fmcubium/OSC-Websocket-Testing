dc2315a90991d4f166f1316f982841524d4bbf73
Andres D <84483578+fmcubium@users.noreply.github.com>
2023-12-12 17:11:37 -0500

    Added remaining helper files/methods

---
+++ server/src/types/Message.ts
@@ -0,0 +1,10 @@
+export interface Message {
+    userId: string
+    msgId: string
+    msgContent: string
+    broadLat: string
+    broadLon: string
+    specificLat: string
+    specificLon: string
+    timeSent: Number
+}
---
+++ server/src/types/User.ts
@@ -0,0 +1,9 @@
+export interface User {
+    displayName: string
+    userId: string
+    avatarUrl: string
+    broadLat: string
+    broadLon: string
+    specificLat: string
+    specificLon: string
+}
